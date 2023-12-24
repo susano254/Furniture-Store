641cc30e5a80ed1f695bee9d6a4c06e13077fc31
Mahmoud Salah <58883594+susano254@users.noreply.github.com>
2023-12-24 15:10:48 +0200

    First Commit

---
+++ app/src/main/java/com/susano/furniturestore/Splash.java
@@ -0,0 +1,26 @@
+package com.susano.furniturestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        FloatingActionButton splash_button = findViewById(R.id.splash_button);
+        splash_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+            }
+        });
+    }
+}
